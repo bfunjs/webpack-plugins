1f01c37ce62dcca7ed7af6a015fe9f41322a64ce
bincheng.sbc <bincheng.sbc@alibaba-inc.com>
2020-08-10 20:05:17 +0800

    feat: vue3 0.0.2

--- packages/webpack4-vue3/src/index.js
+++ packages/webpack4-vue3/src/index.js
@@ -1,5 +1,4 @@
 export { init } from './init';
-export { dev } from './dev';
 
 export const extensions = [ '@bfun/solution-webpack4-standard' ];
 export const required = [ 'vue' ];
--- packages/webpack4-vue3/src/init.js
+++ packages/webpack4-vue3/src/init.js
@@ -1,11 +1,6 @@
-const path = require('path');
 const { createWebpackConfig } = require('@bfun/solution-webpack4-standard');
-const nodeExternals = require('webpack-node-externals');
-const VueLoaderPlugin = require('vue-loader/lib/plugin');
-const VueSSRClientPlugin = require('vue-server-renderer/client-plugin');
-const VueSSRServerPlugin = require('vue-server-renderer/server-plugin');
+const { VueLoaderPlugin } = require('vue-loader');
 
-const { autoDetectJsEntry } = global.common;
 const name = 'vue';
 
 function initCommonConfig(chain, options) {
@@ -35,36 +30,6 @@ function initCommonConfig(chain, options) {
     chain.plugin('vue-loader').use(VueLoaderPlugin);
 }
 
-function initClientConfig(chain, options) {
-    const { client = {} } = options;
-    const { entry = 'entry-client.js', filename = 'ssr-client.json' } = client || {};
-    const clientEntry = autoDetectJsEntry(entry);
-    Object.keys(clientEntry).map(key => chain.entry(key).add(clientEntry[key]));
-
-    initCommonConfig(chain, options);
-    chain.plugin('ssr-client').use(VueSSRClientPlugin, [ { filename } ])
-    // chain.plugins.delete('clean');
-}
-
-
-function initServerConfig(chain, options) {
-    const { server = {} } = options;
-    const { entry = 'entry-server.js', filename = 'ssr-server.json' } = server || {};
-    const serverEntry = autoDetectJsEntry(entry);
-    Object.keys(serverEntry).map(key => chain.entry(key).add(serverEntry[key]));
-
-    initCommonConfig(chain, options);
-
-    chain.plugin('ssr-server').use(VueSSRServerPlugin, [ { filename } ])
-    chain.target('node');
-    chain.output.filename('server-bundle.js');
-    chain.output.libraryTarget('commonjs2');
-    chain.externals(nodeExternals({ whitelist: /\.css$/ }));
-
-    chain.plugins.delete('template');
-    chain.plugins.delete('clean');
-}
-
 export async function init(ctx, next, solutionOptions) {
     const { webpack, options = {} } = ctx.solution || {};
     const { ssr = false } = solutionOptions;
@@ -72,8 +37,6 @@ export async function init(ctx, next, solutionOptions) {
 
     const [ clientConfig, serverConfig ] = webpack;
     if (ssr) {
-        initClientConfig(clientConfig, options);
-        initServerConfig(serverConfig, options);
         ctx.solution.skip.push('@bfun/solution-webpack4-standard:dev');
     } else {
         initCommonConfig(clientConfig, options);
